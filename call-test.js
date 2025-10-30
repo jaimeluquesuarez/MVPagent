c5a49b3f19de01de4238d200a29167bb48b83404
jaimeluquesuarez <jaimeluke4@hotmail.com>
2025-10-30 11:01:13 +0100

    Update call-test.js

--- call-test.js
+++ call-test.js
@@ -1,14 +1,14 @@
 import twilio from "twilio";
 
-const accountSid = "TU_ACCOUNT_SID";
-const authToken = "TU_AUTH_TOKEN";
+const accountSid = "ACd520a9209d5a012e09859cc2bf00d250";
+const authToken = "c2bce8ca2bba5e02b01d970cb538c25a";
 const client = twilio(accountSid, authToken);
 
 client.calls
   .create({
     url: "https://mvpagent.onrender.com/twilio", // tu endpoint
-    to: "+34TU_NUMERO_VERIFICADO",              // tu móvil verificado
-    from: "+15673714886",                       // tu número Twilio (EEUU)
+    to: "+34646786504",              // tu móvil verificado
+    from: "+15416954468"          // tu número Twilio (EEUU)
   })
   .then(call => console.log("Llamada iniciada:", call.sid))
   .catch(err => console.error("Error:", err));
